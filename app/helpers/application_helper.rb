1db6f7876972f722437e8ccfdd1e62bb002877fa
Matt Engledowl <mengledowl@gmail.com>
2016-08-01 23:09:44 -0500

    added dynamic titles to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Job Hackability"
+    unless page_title.empty?; return "#{base_title} | #{page_title}"; end
+    base_title
+  end
 end
