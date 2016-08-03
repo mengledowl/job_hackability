bd53cabfb02318469cdd63186320ad2894b35cd6
Matt Engledowl <mengledowl@gmail.com>
2016-08-02 21:43:58 -0500

    added flash and error messages. created views for JobListing#new/edit.

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -5,20 +5,50 @@ class JobListingsController < ApplicationController
   end
 
   def new
+    @job_listing = JobListing.new
   end
 
   def create
+    @job_listing = JobListing.new(listing_params)
+
+    if @job_listing.save
+      redirect_to @job_listing
+    else
+      flash.now[:error] = 'Could not create job listing'
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+    @job_listing = JobListing.find(params[:id])
   end
 
   def update
+    @job_listing = JobListing.find(params[:id])
+
+    if @job_listing.update(edit_listing_params)
+      redirect_to @job_listing
+    else
+      flash.now[:error] = 'Could not update job listing'
+      render :edit
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def listing_params
+    params.require(:job_listing).permit(:url, :title, :description, :apply_link, :resume_link, :applied_at,
+                                        :cover_letter_link, :favorite, :position, :posted_date, :company_website, :company, :apply_details)
+  end
+
+  def edit_listing_params
+    params.require(:job_listing).permit(:title, :description, :apply_link, :resume_link, :applied_at,
+                                        :cover_letter_link, :favorite, :position, :posted_date, :company_website, :company, :apply_details)
   end
 end
