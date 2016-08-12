cca9c3e0cb15d96a7bbf11b5fd5b8a00bba15e54
Matt Engledowl <mengledowl@gmail.com>
2016-08-11 21:11:23 -0500

    Added validation to interviews for how_it_went

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,7 @@
 class Interview < ActiveRecord::Base
   belongs_to :job_listing
   has_many :comments
+
+  validates_inclusion_of :how_it_went, in: 1..3, allow_nil: true, message: 'should be between 1 and 3'
+
 end
--- test/models/interview_test.rb
+++ test/models/interview_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class InterviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should only allow 1-3 for how_it_went" do
+    interview_0 = Interview.new(how_it_went: 0)
+    interview_negative = Interview.new(how_it_went: -1)
+    interview_1 = Interview.new(how_it_went: 1)
+    interview_3 = Interview.new(how_it_went: 3)
+    interview_4 = Interview.new(how_it_went: 4)
+
+    assert_not interview_0.valid?
+    assert_not interview_negative.valid?
+    assert interview_1.valid?
+    assert interview_3.valid?
+    assert_not interview_4.valid?
+  end
+
+  test "should allow how_it_went to be nil" do
+    interview = Interview.new
+
+    assert interview.valid?
+  end
 end
